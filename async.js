35897174e2adc695d58c93bd0d5947e739e63ca5
Eric Casey <ericcasey509@gmail.com>
2016-09-22 20:02:21 +0000

    adding profit checker

--- async.js
+++ async.js
@@ -1,5 +1,4 @@
 for (var i = 0; i < 3; i++) {
   var counter = 0;
-  var timeout = 1000;
   setTimeout(function() { console.log(counter); counter++;}, (1000 * i) );
 }
--- maxProfit.js
+++ maxProfit.js
@@ -1,12 +1,24 @@
-function maxProfit (ticker) {
-  var min = Math.min(...ticker);
-  var max = Math.max(...ticker);
-  var sorted = ticker.sort();
-  console.log(min, max, sorted);
-  
+function maxProfit(ticker) {
 
+  var buyMargins = {};
+  for (var i = 0; i <= ticker.length - 1; i += 1) {
+    var currentIndex = i;
+    var currentBuy = ticker[i]
+    buyMargins[currentBuy] = [];
+    for (var b = i + 1; b <= ticker.length - 1; b += 1) {
+      var difference = ticker[b] - ticker[currentIndex];
+      buyMargins[currentBuy].push(difference);
+    }
+  } // end of looping thru ticker
+  var allMaxMargins = [];
+  for (buys in buyMargins) {
+    var currentMax = Math.max(...buyMargins[buys])
+    if (currentMax > 1) {
+      allMaxMargins.push(currentMax)
+    }
+  }
+  var profits = allMaxMargins.sort()
+  return allMaxMargins[0]
 };
-
-
 maxProfit([45, 24, 35, 31, 40, 38, 11])
- //should return $16 from 24 to 40
+  //should return $16 from 24 to 40
